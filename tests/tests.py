63c5405850b105ffd5ed466333ebff9fca6286bb
Mica78 <mica78@yandex.ru>
2023-06-29 10:23:58 +0400

    correction tests

--- tests/tests.py
+++ tests/tests.py
@@ -3,23 +3,6 @@ from main import get_unique_names, get_top_3, get_course_array, courses, mentors
 
 
 class MyTestCase(unittest.TestCase):
-    def test_get_uniq_name(self):
-        self.assertEqual(get_unique_names(), "Уникальные имена преподавателей: Адилет, Азамат, Александр, Алексей, "
-                                             "Алена, Анатолий, Анна, Антон, Вадим, Валерий, Владимир, Денис, Дмитрий, "
-                                             "Евгений, Елена, Иван, Илья, Кирилл, Константин, Максим, Михаил, Никита, "
-                                             "Николай, Олег, Павел, Ринат, Роман, Сергей, Татьяна, Тимур, Филипп, "
-                                             "Эдгар, Юрий")
-
-    def test_get_top3(self):
-        self.assertEqual(get_top_3(), "Александр: 10 раз(а), Евгений: 5 раз(а), Максим: 4 раз(а)")
-
-    def test_get_course_array(self):
-        self.assertEqual(get_course_array(),
-                         (
-                             'Порядок курсов по длительности: [2, 0, 1, 3]',
-                             'Порядок курсов по количеству преподавателей: [0, 3, 2, 1]'
-                         )
-                         )
 
     def test_courses(self):
         self.assertIsInstance(courses, list)
@@ -38,6 +21,24 @@ class MyTestCase(unittest.TestCase):
         self.assertIsInstance(durations, list)
         for duration in durations:
             self.assertIsInstance(duration, int)
+
+    def test_get_uniq_name(self):
+        self.assertEqual(get_unique_names(), "Уникальные имена преподавателей: Адилет, Азамат, Александр, Алексей, "
+                                             "Алена, Анатолий, Анна, Антон, Вадим, Валерий, Владимир, Денис, Дмитрий, "
+                                             "Евгений, Елена, Иван, Илья, Кирилл, Константин, Максим, Михаил, Никита, "
+                                             "Николай, Олег, Павел, Ринат, Роман, Сергей, Татьяна, Тимур, Филипп, "
+                                             "Эдгар, Юрий")
+
+    def test_get_top3(self):
+        self.assertEqual(get_top_3(), "Александр: 10 раз(а), Евгений: 5 раз(а), Максим: 4 раз(а)")
+
+    def test_get_course_array(self):
+        self.assertEqual(get_course_array(),
+                         (
+                             'Порядок курсов по длительности: [2, 0, 1, 3]',
+                             'Порядок курсов по количеству преподавателей: [0, 3, 2, 1]'
+                         )
+                         )
 
 
 if __name__ == '__main__':
